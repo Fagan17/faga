0e965b99df07bc027e26b53e89f28b468cebcaf4
Fagan17 <rodantype3@gmail.com>
2025-11-25 19:50:09 +0400

    updatee

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-const name = "Fagan";
-console.log(name);
+let fullName = "Fagan Maharramov";
+console.log(fullName.split(" ")[0][0]);
+console.log(fullName.split(" ")[1][0]);
+let userName = "Fagan Maharramov";
+let cut = (userName.trim().split(" "));
+let firstName = cut[0]
+let lastName = cut[1]
+let reversed = lastName + " " + firstName;
+console.log(reversed);
