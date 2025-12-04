734c6d4f33070f83fe2bab73c916c1e335deae3d
Fagan17 <rodantype3@gmail.com>
2025-12-04 19:11:06 +0400

    end

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-let a = 20
-console.log(a == 10)
-console.log(a != 20)
-console.log(a !== 20)
-console.log( a === 10)
-console.log( a <= 15)
+let age = prompt("Yasinizi daxil edin") 
+if(age < 18){
+    console.log("icaze verilmedi ")
+}else{
+    console.log("Icaze verildi")
+}
+let fullName = prompt("Adinizi Soyadinizi daxil edin")
+console.log("Salam,!",fullName.split(" ")[1])
+
