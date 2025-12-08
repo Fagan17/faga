c892174c656c95a9407b2134913f3bfff6c6b719
Fagan17 <rodantype3@gmail.com>
2025-12-08 19:07:57 +0400

    ad

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-let age = prompt("Yasinizi daxil edin") 
-if(age < 18){
-    console.log("icaze verilmedi ")
-}else{
-    console.log("Icaze verildi")
+let month = Number(prompt("Neçenci aydir?"))
+switch(month){
+    case 1:
+        console.log("Yanvar")
+        break
+        case 2:
+        console.log("Fevral")
+        break
+         case 3:
+        console.log("Mart") 
+         break
+        case 4:
+        console.log("Aprel")
+         break
+        case 5:
+        console.log("May")
+        break
+         case 6:
+        console.log("Iyun")
+        break
+         case 7:
+        console.log("Iyul")
+        break 
+        case 8:
+        console.log("Avqust")
+        break 
+        case 9:
+        console.log("Sentyabr")
+        break 
+        case 10:
+        console.log("Oktyabr")
+        break
+         case 11:
+        console.log("Noyabr")
+        break 
+        case 12:
+        console.log("Dekabr")
+        break
 }
-let fullName = prompt("Adinizi Soyadinizi daxil edin")
-console.log("Salam,!",fullName.split(" ")[1])
-
+let date = Number(prompt("Ayin tarixi neçedir? Misal: 2025.12.30"))
