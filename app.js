819f7273b649399143b4144793a76c90607f80ac
Fagan17 <rodantype3@gmail.com>
2025-12-11 01:13:17 +0400

    sd

--- app.js
+++ app.js
@@ -1,40 +1,4 @@
-let month = Number(prompt("Neçenci aydir?"))
-switch(month){
-    case 1:
-        console.log("Yanvar")
-        break
-        case 2:
-        console.log("Fevral")
-        break
-         case 3:
-        console.log("Mart") 
-         break
-        case 4:
-        console.log("Aprel")
-         break
-        case 5:
-        console.log("May")
-        break
-         case 6:
-        console.log("Iyun")
-        break
-         case 7:
-        console.log("Iyul")
-        break 
-        case 8:
-        console.log("Avqust")
-        break 
-        case 9:
-        console.log("Sentyabr")
-        break 
-        case 10:
-        console.log("Oktyabr")
-        break
-         case 11:
-        console.log("Noyabr")
-        break 
-        case 12:
-        console.log("Dekabr")
-        break
+function calc(rubl,dollar) {
+    console.log(rubl + (dollar * 78))
 }
-let date = Number(prompt("Ayin tarixi neçedir? Misal: 2025.12.30"))
+calc(5,2)
