313a5ed79824adebb8683cb36b9010e678315ae1
Fagan17 <rodantype3@gmail.com>
2026-01-08 16:06:28 +0400

    bvvbv

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-let arr = [1,2,3,4,5];
-let newA = [...arr];
-for (let i = 0; i < newA.length; i++){
-    newA[i] *=2
+
+function reqemler(arr) {
+  let musbet = 0;
+  let menfi = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      musbet++;
+    } else {
+      menfi++;
+    }
+  }
+  return {
+    even: musbet,
+    odd: menfi
+  };
 }
-console.log(arr)
-console.log(newA)
+console.log(reqemler([1, 2, 3, 4, 5, 6]));
