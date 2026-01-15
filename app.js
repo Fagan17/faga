45b7b66a43b40cb3482784ce53f52714bb7b61eb
Fagan17 <rodantype3@gmail.com>
2026-01-15 16:58:59 +0400

    hm

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-let arr = [1,2,3,4,5,6,7,8,9];
-let sum = 0;
-
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] % 2 !== 0) {
-    sum += arr[i];
-  }
-}
-
-console.log(sum); 
-
-
-let a = ["js", "front", "array","css"];
-let result = [];
-
-for (let i = 0; i < a.length; i++) {
-  let reversed = a[i]
-    .split("")
-    .sort((a, b) => -1)
-    .join("");
-
-  result.push(reversed);
-}
-
-console.log(result); 
+let user = {
+  name: "Fagan",
+  age: 28,
+ retirementAge: 65,
+ setAge(newAge) {
+this.age = newAge;
+ },
+ getYearsBeforeRetirement(){
+  return this.retirementAge - this.age;
+ }
+};
+let yearsLeft = user.getYearsBeforeRetirement();
+console.log("Pensiyaya qalan il",yearsLeft)
