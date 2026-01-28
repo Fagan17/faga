f7d63841765c3900d30fb0b540165a1167e90426
Fagan17 <rodantype3@gmail.com>
2026-01-29 03:16:29 +0400

    homeworkk

--- app.js
+++ app.js
@@ -1,50 +1,28 @@
-
-const data = [
-{ name: "Bitcoin", ticker: "BTC", price: 9685, change: 2.83 },
-{ name: "Ethereum", ticker: "ETH", price: 210.5, change: 6.17 },
-{ name: "Ripple", ticker: "XRP", price: 0.2812, change: -2.47 },
-{ name: "Litecoin", ticker: "LTC", price: 74.935, change: 4.25 },
-{ name: "Sitecoin", ticker: "LTC", price: 74.585, change: 3.25 },
-{ name: "Mitecoin", ticker: "LTC", price: 74.565, change: -2.25 },
-{ name: "Yitecoin", ticker: "LTC", price: 75.935, change: 4.25 },
-{ name: "Tezos", ticker: "XTZ", price: 2.1247, change: -6.12 }
-];
-
-const container = document.getElementById("app");
+document.addEventListener("keyup", (e) =>{
+    console.log(e.key)
+})
+  
 
 
-const table = document.createElement("table");
 
 
-table.innerHTML = `
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Ticker</th>
-      <th>Price</th>
-      <th>24h Change</th>
-    </tr>
-  </thead>
-`;
+  let inp = document.querySelector("input");
+   let btn = document.querySelector("button");
+    btn.addEventListener("click", () =>{
+        console.log(inp.value)
+    })
 
-const tbody = document.createElement("tbody");
 
-data.forEach(item => {
-  const tr = document.createElement("tr");
 
-  const changeClass = item.change >= 0 ? "positive" : "negative";
 
-  tr.innerHTML = `
-    <td>${item.name}</td>
-    <td>${item.ticker}</td>
-    <td>${item.price}</td>
-    <td class="${changeClass}">
-      ${item.change}%
-    </td>
-  `;
+  document.getElementById("myForm").addEventListener("submit", function (e) {
+      e.preventDefault(); 
 
-  tbody.appendChild(tr);
-});
+      let password = document.getElementById("password").value;
 
-table.appendChild(tbody);
-container.appendChild(table);
+      if (password.length > 5) {
+        console.log("İcazə verildi");
+      } else {
+        console.log("Giriş qadağandır");
+      }
+    });
